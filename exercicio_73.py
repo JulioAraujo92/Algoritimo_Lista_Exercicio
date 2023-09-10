e18c6f1be521756b878b8caf6432863e28d78ea6
Julio Cesar <118136237+JulioAraujo92@users.noreply.github.com>
2023-09-10 17:09:38 -0300

    atualizacao exer_73

--- exercicio_73.py
+++ exercicio_73.py
@@ -1,5 +1,5 @@
 num=float(input("\nEntre com um número com parte fracionária: "))
 numi=round((num-0.5))
 numfrac=num-numi
-numa=round(num)
+numa=round(num+0.00001)
 print(f"\nParte inteira: {numi}\nParte fracionária: {numfrac:.3f}\nNúmero arredondado: {numa}\n")
