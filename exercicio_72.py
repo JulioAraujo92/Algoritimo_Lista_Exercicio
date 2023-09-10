1c51404b6ec604430081bf9f9bdff895acbd13bc
Julio Cesar <118136237+JulioAraujo92@users.noreply.github.com>
2023-09-10 17:44:58 -0300

    atualização exer_72

--- exercicio_72.py
+++ exercicio_72.py
@@ -2,4 +2,4 @@ deposito=float(input("\nEntre com depósito: "))
 taxa=float(input("\nEntre com a taxa de juros: "))
 valor=deposito*taxa/100
 total=deposito+valor
-print(f"\nRendimentos: {total:.2f}\nTotal: {total:.2f}\n")
+print(f"\nRendimentos: {valor:.2f}\nTotal: {total:.2f}\n")
